90cd643f6b6642d0112daa196a6449556d6e295e
Carlos Abel Mejía López <46359061+CucoML17@users.noreply.github.com>
2025-05-25 19:44:34 -0600

    Delete proyectohoteles directory

--- proyectohoteles/src/main/java/carlos/mejia/proyectohoteles/repository/PerfilRepository.java
+++
@@ -1,9 +0,0 @@
-package carlos.mejia.proyectohoteles.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import carlos.mejia.proyectohoteles.model.Perfil;
-
-public interface PerfilRepository extends JpaRepository<Perfil, Integer> {
-
-}
--- proyectohoteles/src/main/java/carlos/mejia/proyectohoteles/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package carlos.mejia.proyectohoteles.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import carlos.mejia.proyectohoteles.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-	Usuario findByUsername(String username);
-}
